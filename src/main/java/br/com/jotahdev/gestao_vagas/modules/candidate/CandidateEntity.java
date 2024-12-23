6acba0caf9240612a6b5579049f2989f0f90af28
jotahdev <joaomanoelskai@gmail.com>
2024-12-22 22:03:25 -0300

    Inicializando validações

--- src/main/java/br/com/jotahdev/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/jotahdev/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -2,6 +2,10 @@ package br.com.jotahdev.gestao_vagas.modules.candidate;
 
 import java.util.UUID;
 
+import org.hibernate.validator.constraints.Length;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
@@ -9,8 +13,14 @@ public class CandidateEntity {
     
     private UUID id;
     private String name;
+
+    @Pattern(regexp = "^(?!\\s*S).+", message = "O nome de usuário não pode conter espaços.")
     private String username;
+
+    @Email(message = "Insira um e-mail válido para continuar.")
     private String email;
+
+    @Length(min = 10, max = 100)
     private String password;
     private String description;
     private String curriculum;
