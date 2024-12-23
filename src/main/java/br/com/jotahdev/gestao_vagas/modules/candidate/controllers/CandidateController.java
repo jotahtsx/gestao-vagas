ae3bd09903254a10fa03e96542613f282f875cd8
jotahdev <joaomanoelskai@gmail.com>
2024-12-23 08:01:39 -0300

    Criando repository e cadastrando primeiro candidato

--- src/main/java/br/com/jotahdev/gestao_vagas/modules/candidate/controllers/CandidateController.java
+++ src/main/java/br/com/jotahdev/gestao_vagas/modules/candidate/controllers/CandidateController.java
@@ -1,21 +1,24 @@
 package br.com.jotahdev.gestao_vagas.modules.candidate.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.jotahdev.gestao_vagas.modules.candidate.CandidateEntity;
+import br.com.jotahdev.gestao_vagas.modules.candidate.CandidateRepository;
 import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/candidato")
 public class CandidateController {
 
-    @PostMapping("/")
-    public void create(@Valid @RequestBody CandidateEntity candidateEntity) {
-        System.out.println("Candidato");
-        System.out.println(candidateEntity.getEmail());
-    }
+    @Autowired
+    private CandidateRepository candidateRepository;
 
+    @PostMapping("/")
+    public CandidateEntity create(@Valid @RequestBody CandidateEntity candidateEntity) {
+        return this.candidateRepository.save(candidateEntity);
+    }
 }
