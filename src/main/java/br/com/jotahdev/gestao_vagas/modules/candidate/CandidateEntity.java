2eb15dc4c1dd0b5c93d2ad2763d38c392278c389
jotahdev <joaomanoelskai@gmail.com>
2024-12-22 23:21:30 -0300

    Corrigindo validações e inicializando as simples

--- src/main/java/br/com/jotahdev/gestao_vagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/jotahdev/gestao_vagas/modules/candidate/CandidateEntity.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 import org.hibernate.validator.constraints.Length;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
@@ -14,13 +15,14 @@ public class CandidateEntity {
     private UUID id;
     private String name;
 
-    @Pattern(regexp = "^(?!\\s*S).+", message = "O nome de usuário não pode conter espaços.")
+    @NotBlank()
+    @Pattern(regexp = "\\S+", message = "O nome de usuário não pode conter espaços.")
     private String username;
 
     @Email(message = "Insira um e-mail válido para continuar.")
     private String email;
 
-    @Length(min = 10, max = 100)
+    @Length(min = 8, max = 100, message = "A senha deve ter entre 8 e 100 caracteres.")
     private String password;
     private String description;
     private String curriculum;
